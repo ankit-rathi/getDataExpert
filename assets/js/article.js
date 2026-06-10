f135e1f3e944232c883717d67817b67416671e00
Ankit Rathi <rathi.ankit@gmail.com>
2026-06-10 17:24:48 +0530

    Update article.js

--- assets/js/article.js
+++ assets/js/article.js
@@ -1,25 +1,67 @@
-fetch(
+let relatedTopics = {};
 
-"/data/related-topics.json"
+fetch("/sketch-intelligence/data/related-topics.json")
 
-)
+.then(response => response.json())
 
-.then(
+.then(data => {
 
-response => response.json()
+    relatedTopics = data;
 
-)
+    renderConnectedConcepts();
 
-.then(
+});
 
-related => {
 
-    showRelatedTopics(
+function prettify(text){
 
-        related[currentTopic]
-
-    );
+    return text
+            .replaceAll("-", " ")
+            .replace(/\b\w/g,
+                c => c.toUpperCase());
 
 }
 
-);
+
+function renderConnectedConcepts() {
+
+    let slug = document.body.dataset.slug;
+
+    let topics = relatedTopics[slug];
+
+    if (!topics || topics.length === 0)
+        return;
+
+    let html = `
+
+        <h2>
+            Connected Concepts
+        </h2>
+
+        <div class="concept-cards">
+
+    `;
+
+    topics.forEach(topic => {
+
+        html += `
+
+            <a
+                class="concept-card"
+                href="/sketch-intelligence/topics/${topic}/">
+
+                ${prettify(topic)}
+
+            </a>
+
+        `;
+
+    });
+
+    html += "</div>";
+
+    document
+        .getElementById("connected-concepts")
+        .innerHTML = html;
+
+}
